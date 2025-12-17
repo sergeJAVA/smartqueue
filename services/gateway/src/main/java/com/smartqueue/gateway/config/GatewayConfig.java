33612607050083803ec64b0bf713eaa4423abb16
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-12-17 19:39:44 +0300

    feat(gateway): Added CorsWebFilter bean.

--- services/gateway/src/main/java/com/smartqueue/gateway/config/GatewayConfig.java
+++ services/gateway/src/main/java/com/smartqueue/gateway/config/GatewayConfig.java
@@ -5,9 +5,27 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder, JwtAuthFilter jwtFilter) {
@@ -15,7 +33,7 @@ public class GatewayConfig {
                 .route("queue", r -> r.path("/api/v1/queue/**")
                         .filters(f -> f.filter(jwtFilter)).uri("lb://QUEUE-SERVICE"))
                 .route("notification", r -> r.path("/ws")
-                        .filters(f -> f.filter(jwtFilter)).uri("lb://NOTIFICATION-SERVICE"))
+                        .filters(f -> f.filter(jwtFilter)).uri("lb:ws://NOTIFICATION-SERVICE"))
                 .build();
     }
 
